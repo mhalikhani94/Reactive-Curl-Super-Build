30648289464473a3d0012b5942e12245054fcf07
MH Alikhani <mhalikhani@DESKTOP-DKVSOC3.localdomain>
2022-09-12 18:16:40 +0430

    Add BlockForst Implementations on cardano api files.
    add required types.
    
    Build Failed.
    Test Failed.

--- src/blockchains/cardano/cardano-api.cpp
+++ src/blockchains/cardano/cardano-api.cpp
@@ -53,3 +53,135 @@ rxcpp::observable<std::vector<ns::RawUtxo>> CardanoApi::get_utxos_of_addresses(
         return results;
     });
 }
+
+rxcpp::observable<long> CardanoApi::get_slot_num(NetworkType network_type)
+{
+    auto worker_thread = rxcpp::observe_on_event_loop();
+
+    return rxcpp::observable<>::create<HttpResponse>([&](rxcpp::subscriber<HttpResponse> out)
+    {
+        try
+        {
+            http_client->build()->Get(network_type.yoroi_api_root + "/blocks/latest")
+            .add_header("project_id", network_type.block_frost_project_id)
+            .process_response([&](const HttpResponse& result)
+            {
+                if (result.is_ok())
+                {
+                    try
+                    {
+                        out.on_next(result);
+                        out.on_completed();
+                    } catch ( ... ) {
+                        out.on_error(std::current_exception());
+                    }
+                } else {
+                    out.on_next(result);
+                    out.on_completed();
+                }
+            })
+            .send_request();
+        }
+        catch(...)
+        {
+            out.on_error(std::current_exception());
+        }
+        
+    }).subscribe_on(worker_thread)
+    .map([&](const HttpResponse& result)
+    {
+        //TODO: parse result.body to get result!
+        long long_result{};
+        return long_result;
+    });
+}
+
+rxcpp::observable<std::string> submit_signed_tx(std::vector<uint8_t> signed_transaction
+    , NetworkType network_type)
+{
+    auto worker_thread = rxcpp::observe_on_event_loop();
+
+    return rxcpp::observable<>::create<HttpResponse>([&](rxcpp::subscriber<HttpResponse> out)
+    {
+        try
+        {
+            http_client->build()->Get(network_type.yoroi_api_root + "/tx/submit")
+            .add_header("Content-Type","application/cbor")
+            .add_header("project_id", network_type.block_frost_project_id)
+            .add_body(signed_transaction)
+            .process_response([&](const HttpResponse& result)
+            {
+                if (result.is_ok())
+                {
+                    try
+                    {
+                        out.on_next(result);
+                        out.on_completed();
+                    } catch ( ... ) {
+                        out.on_error(std::current_exception());
+                    }
+                } else {
+                    out.on_next(result);
+                    out.on_completed();
+                }
+            })
+            .send_request();
+        }
+        catch(...)
+        {
+            out.on_error(std::current_exception());
+        }
+        
+    }).subscribe_on(worker_thread)
+    .map([&](const HttpResponse& result)
+    {
+        //TODO: parse result.body to get result!
+        std::string string_result{};
+        return string_result;
+    });
+}
+
+rxcpp::observable<std::list<SpentTransaction>> retrieve_transactions_desc(std::string address, long page
+    , int offset, NetworkType network_type)
+{
+        auto worker_thread = rxcpp::observe_on_event_loop();
+
+    return rxcpp::observable<>::create<HttpResponse>([&](rxcpp::subscriber<HttpResponse> out)
+    {
+        try
+        {
+            http_client->build()->Get(network_type.yoroi_api_root + "/tx/submit")
+            .add_header("Content-Type","application/cbor")
+            .add_header("project_id", network_type.block_frost_project_id)
+            .add_body(signed_transaction)
+            .process_response([&](const HttpResponse& result)
+            {
+                if (result.is_ok())
+                {
+                    try
+                    {
+                        out.on_next(result);
+                        out.on_completed();
+                    } catch ( ... ) {
+                        out.on_error(std::current_exception());
+                    }
+                } else {
+                    out.on_next(result);
+                    out.on_completed();
+                }
+            })
+            .send_request();
+        }
+        catch(...)
+        {
+            out.on_error(std::current_exception());
+        }
+        
+    }).subscribe_on(worker_thread)
+    .map([&](const HttpResponse& result)
+    {
+        //TODO: parse result.body to get result!
+        SpentTransaction spend_transaction_result{};
+        return spend_transaction_result;
+    });
+}
--- src/blockchains/cardano/cardano-types.hpp
+++ src/blockchains/cardano/cardano-types.hpp
@@ -7,14 +7,204 @@ using json = nlohmann::json;
 #include <string>
 #include <list>
 
+inline constexpr std::string kBlockFrostMainNetUrl{"https://cardano-mainnet.blockfrost.io/api/v0"};
+inline constexpr std::string kBlockFrostTestNetUrl{"https://cardano-testnet.blockfrost.io/api/v0"};
+
 //TODO: maybe we need to change these public members to private.
 struct NetworkType
 {
+    explicit NetworkType(int id_, std::string yoroi_api_root_, std::string block_frost_api_root_,
+        std::string block_frost_project_id_, std::string explorer_trx_url_): id(id_)
+        , yoroi_api_root(std::move(yoroi_api_root_))
+        , block_frost_api_root(std::move(block_frost_api_root_))
+        , block_frost_project_id(std::move(block_frost_project_id_))
+        , explorer_trx_url(std::move(explorer_trx_url_))
+    {
+    }
+
     int id;
 	std::string yoroi_api_root;
 	std::string block_frost_api_root;
-	std::string get_block_frost_project_id;
+	std::string block_frost_project_id;
 	std::string explorer_trx_url;
+};
+
+inline constexpr NetworkType kShelleyMainNet
+    { 1
+    , "https://iohk-mainnet.yoroiwallet.com/api"
+    , kBlockFrostMainNetUrl
+    , "mainnetrCopBXtdL4ewkO03TzdIutti9g1kkwiT"
+    , "https://explorer.cardano.org/en/transaction?id="
+    };
+
+inline constexpr NetworkType kShelleyTestNet
+    { 0
+    , "https://testnet-backend.yoroiwallet.com/api"
+    , kBlockFrostTestNetUrl
+    , "testnetZ7v804pEgYIUL8Q4aMP3BlFRDqlbV3aH"
+    , "https://explorer.cardano-testnet.iohkdev.io/en/transaction?id="
+    };
+
+struct BigInteger //TODO: create file common types for all blockchains.
+{
+	double big_integer;
+
+    std::string to_string()
+    {
+        return std::string{};
+    }
+};
+
+struct TxoAmount
+{
+    BigInteger big_integer;
+    std::string asset_id;
+};
+
+struct TxOutputAmount
+{
+    std::string unit;
+    std::string quantity;
+};
+
+struct TxoInOut
+{
+    std::string address;
+    std::list<TxoAmount> amounts;
+};
+
+struct TxContentOutputAmount
+{
+    std::string unit;
+    std::string quantity;
+};
+
+struct TxContentUtxoInputs
+{
+    std::string address;
+    std::list<TxContentOutputAmount> amount;
+};
+
+struct TxContentUtxoOutputs
+{
+    std::string address;
+    std::list<TxContentOutputAmount> amount;
+};
+
+struct TransactionContent
+{
+    std::string block;
+    int block_height;
+    int slot;
+    int index;
+    std::list<TxOutputAmount> output_amount;
+    std::string fees;
+    std::string deposit;
+    int size;
+    std::string invalid_before;
+    std::string invalid_hereafter;
+    int utxo_count;
+    int withdrawal_count;
+    int delegation_count;
+    int stake_cert_count;
+    int pool_update_count;
+    int pool_retire_count;
+};
+
+struct SpentTransaction
+{
+	std::string hash;
+	std::string block;
+	uint32_t block_tTime;
+	int block_height;
+	int slot;
+	int index;
+	std::list<TxoInOut> inputs;
+	std::list<TxoInOut> outputs;
+	std::string fees;
+	std::string deposit;
+	int size;
+	std::string invalid_before;
+	std::string invalid_hereafter;
+	int utxo_count;
+	int withdrawal_count;
+	int delegation_count;
+	int stake_cert_count;
+	int pool_update_count;
+	int pool_retire_count;
+
+    SpentTransaction(std::string tx_hash, std::list<TxContentUtxoInputs> inputs
+        , std::list<TxContentUtxoOutputs> outputs, uint32_t block_time_, TransactionContent tx_content)
+    {
+        hash = tx_hash;
+		block = tx_content.block;
+		blockTime = block_time_;
+		blockHeight = tx_content.block_height;
+		slot = tx_content.slot;
+		index = tx_content.index;
+		init_inputs(inputs);
+		init_outputs(outputs);
+		fees = tx_content.fees;
+		deposit = tx_content.deposit;
+		size = tx_content.size;
+		invalidBefore = tx_content.invalid_before;
+		invalidHereafter = tx_content.invalid_hereafter;
+		utxoCount = tx_content.utxo_count;
+		withdrawalCount = tx_content.withdrawal_count;
+		delegationCount = tx_content.delegation_count;
+		stakeCertCount = tx_content.stake_cert_count;
+		poolUpdateCount = tx_content.pool_update_count;
+		poolRetireCount = tx_content.pool_retire_count;
+    }
+
+	void init_inputs(std::list<TxContentUtxoInputs> externalInputs)
+    {
+		for (TxContentUtxoInputs exInput : externalInputs)
+        {
+			std::list<TxoAmount> amounts = get_txo_amounts(exInput.amount);
+			inputs.emplace_back(exInput.address, amounts);
+		}
+	}
+
+	void init_outputs(std::list<TxContentUtxoOutputs> externalOutputs)
+    {
+		for (TxContentUtxoOutputs exOutput : externalOutputs)
+        {
+			std::list<TxoAmount> amounts = get_txo_amounts(exOutput.amount);
+			outputs.emplace_back(exOutput.address, amounts);
+		}
+	}
+
+	std::list<TxoAmount> get_txo_amounts(std::list<TxContentOutputAmount> exAmounts)
+    {
+        std::list<TxoAmount> amounts;
+		for(TxContentOutputAmount exAmount : exAmounts)
+        {
+			amounts.emplace_back(BigInteger{}, exAmount.unit);
+		}
+		return amounts;
+	}
+
+};
+
+
+class Block
+{
+    long time;
+    long height;
+    std::string hash;
+    long slot;
+    int epoch;
+    int epoch_slot;
+    std::string slot_leader;
+    int size;
+    int tx_count;
+    std::string output;
+    std::string fees;
+    std::string block_vrf;
+    std::string previous_block;
+    std::string next_block;
+    int confirmations;
 };
 
 namespace ns
@@ -54,31 +244,6 @@ namespace ns
 
 
 /* Garbage Section */
-
-// struct TxoAmount
-// {
-// private:
-//     uint64_t big_integer; // TODO: change with Java BigObject Class
-//     std::string asset_id;
-
-// public:
-//     uint64_t get_big_integer(){return big_integer;}
-
-//     std::string get_asset_id(){return asset_id;}
-// };
-
-// struct TxoInOut
-// {
-// private:
-//     std::string address;
-//     std::list<TxoAmount> amounts;
-
-// public:
-//     std::string get_address(){return address;}
-
-//     std::list<TxoAmount> get_amounts(){return amounts;}
-// };
-
 
 // struct SpentTransaction
 // {
